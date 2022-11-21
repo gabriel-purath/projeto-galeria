67c2ba7ad02fb6ada32d8419e7c978ba9bd19281
Gabriel Rangel <103848553+gabriel-purath@users.noreply.github.com>
2022-11-21 15:03:46 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// ARQUIVOS SASS
+import './scss/index.scss'
+
+// Dependências
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
